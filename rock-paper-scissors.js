0a45324020bed805510f25ab4bcbe8623afdc15f
leinadsened <denes.daniel93@gmail.com>
2022-12-01 15:16:07 +0100

    add scores and game ending messages

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -36,24 +36,58 @@ function playRound(playerSelection, computerSelection){
 }
 
 function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let scoreDiv = document.getElementById("scores");
+    let result = document.getElementById("result");
+    let rockButton = document.getElementById("rock");
+    rockButton.addEventListener("click", function(){
+        let thisRoundsResult = playRound("rock", getComputerChoice());
+        result.textContent = thisRoundsResult;
+        if (thisRoundsResult == "Player wins!"){playerScore++;}
+        else if (thisRoundsResult == "AI wins!") {computerScore++;}
+        if (playerScore >= 5){
+            scoreDiv.textContent = "You have beaten the AI, you must be very smart!"
+        } else if (computerScore >= 5){
+            scoreDiv.textContent = "The computer must have had luck, it won this time."
+        } else {
+            scoreDiv.textContent = "Player: " + playerScore + " Computers score: " + computerScore;
+        }    });
+    let paperButton = document.getElementById("paper");
+    paperButton.addEventListener("click", function(){
+        let thisRoundsResult = playRound("paper", getComputerChoice());
+        result.textContent = thisRoundsResult;
+        if (thisRoundsResult == "Player wins!"){playerScore++;}
+        else if (thisRoundsResult == "AI wins!") {computerScore++;}
+        if (playerScore >= 5){
+            scoreDiv.textContent = "You have beaten the AI, you must be very smart!"
+        } else if (computerScore >= 5){
+            scoreDiv.textContent = "The computer must have had luck, it won this time."
+        } else {
+            scoreDiv.textContent = "Player: " + playerScore + " Computers score: " + computerScore;
+        }    });
+    let scissorsButton = document.getElementById("scissors");
+    scissorsButton.addEventListener("click", function(){
+        let thisRoundsResult = playRound("scissors", getComputerChoice());
+        result.textContent = thisRoundsResult;
+        if (thisRoundsResult == "Player wins!"){playerScore++;}
+        else if (thisRoundsResult == "AI wins!") {computerScore++;}
+        if (playerScore >= 5){
+            scoreDiv.textContent = "You have beaten the AI, you must be very smart!"
+        } else if (computerScore >= 5){
+            scoreDiv.textContent = "The computer must have had luck, it won this time."
+        } else {
+            scoreDiv.textContent = "Player: " + playerScore + " Computers score: " + computerScore;
+        }
+    });
 
 }
+
 
 function getPlayerChoice(){
     let input = prompt("Please choose rock, paper or scissors!");
     let choice = input.toLowerCase();
     return choice;
 }
-let result = document.getElementById("result");
-let rockButton = document.getElementById("rock");
-rockButton.addEventListener("click", function(){
-     result.textContent = playRound("rock", getComputerChoice());
-});
-let paperButton = document.getElementById("paper");
-paperButton.addEventListener("click", function(){
-    result.textContent = playRound("paper", getComputerChoice());
-});
-let scissorsButton = document.getElementById("scissors");
-scissorsButton.addEventListener("click", function(){
-    result.textContent = playRound("scissors", getComputerChoice());
-});
+
+game();
